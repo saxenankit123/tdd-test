6b798453ff5e01572d062d6eae30ecb602aaa512
Ankit Saxena <saxenankit@gmail.com>
2024-10-14 20:15:49 +0530

    corrected test case

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 export function validateName(name) {
-    if (name.length > 10) {
+    if (name.length > 20) {
         const err = new Error("User cannot be registered because name is more than 20 chars long")
         err.statusCode = 400
         throw err
